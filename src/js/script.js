7bb81ac71cc63ccf6b43c93a432948d28f90c7d2
luyz-gusta <luizgustavo.sp2020@gmail.com>
2024-05-03 23:01:17 -0300

    Contato finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -12,8 +12,9 @@ const colorsGradient = ['linear-gradient(180deg, #AEBDD1 0%, #3C495A 100%)', 'li
 
 const colorText = ['#333333', '#7E7A77', '#333333']
 const colorH1 = ['#fff', '#fff', '#131E2F']
+const colorsContact = ['#2e6a8d', '#FF5E1E', '#2e6a8d']
 
-const changeBackground = () => {
+const changeBackgroundIndex = () => {
     const main = document.querySelector('main')
     const footer = document.querySelector('footer')
     const header = document.querySelector('header')
@@ -65,6 +66,56 @@ const changeBackground = () => {
         index++
     }
 }
+
+const changeBackgroundContact = () => {
+    const main = document.querySelector('main')
+    const btnColor = document.getElementById('btnColor')
+    const footer = document.querySelector('footer')
+    const header = document.querySelector('header')
+    const h1 = document.querySelectorAll('h1')
+    const btn = document.getElementById('btn')
+
+    btnColor.style.background = colorsOne[index]
+    main.style.background = colorsOne[index]
+    footer.style.background = colorsThree[index]
+    header.style.background = colorsThree[index]
+    btn.style.background = colorsContact[index]
+    h1[1].style.color = colorsContact[index]
+
+    const input = document.querySelectorAll('.input')
+    const label = document.querySelectorAll('.label')
+    input[0].addEventListener('focus', () => {
+        label[0].style.color = colorsContact[index == 0 ? index : index - 1];
+    })
+    input[1].addEventListener('focus', () => {
+        label[1].style.color = colorsContact[index == 0 ? index : index - 1];
+    })
+    input[2].addEventListener('focus', () => {
+        label[2].style.color = colorsContact[index == 0 ? index : index - 1];
+    })
+
+    input[0].addEventListener('blur', () => {
+        label[0].style.color = '#000';
+    })
+    input[1].addEventListener('blur', () => {
+        label[1].style.color = '#000';
+    })
+    input[2].addEventListener('blur', () => {
+        label[2].style.color = '#000';
+    })
+
+    if (index == 2) {
+        btnColor.style.color = "#000000"
+        btnColor.style.border = "2px solid #000000"
+        index = 0
+    } else {
+        btnColor.style.border = "2px solid #ffff"
+        btnColor.style.color = "#FFFFFF"
+        index++
+    }
+}
+
+
 
 const slideShow = () => {
     const banner = document.getElementById('banner')
