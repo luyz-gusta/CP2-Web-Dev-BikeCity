c3215931166e15d2545f15eba0133cfbf4ee852f
luyz-gusta <luizgustavo.sp2020@gmail.com>
2024-05-04 16:29:07 -0300

    Login finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -14,6 +14,23 @@ const colorText = ['#333333', '#7E7A77', '#333333']
 const colorH1 = ['#fff', '#fff', '#131E2F']
 const colorsContact = ['#2e6a8d', '#FF5E1E', '#2e6a8d']
 
+const validar = () => {
+    const data = {
+        user: 'quiz',
+        password: 'gamestart'
+    }
+
+    const user = document.getElementById('user-box').value
+    const password = document.getElementById('password-box').value
+
+    if (user == data.user && password == data.password) {
+        alert('foi')
+        window.open("./quiz.html")
+    } else {
+        alert('Usuário ou Senha incorretos')
+    }
+}
+
 const changeBackgroundIndex = () => {
     const main = document.querySelector('main')
     const footer = document.querySelector('footer')
@@ -51,9 +68,17 @@ const changeBackgroundIndex = () => {
 
     btnBanner.addEventListener('mouseleave', () => {
         btnBanner.style.background = 'none'
+        btnBanner.style.boxShadow = 'none'
     })
     btnBanner.addEventListener('mouseenter', () => {
-        btnBanner.style.background = colorsTwo[index == 0 ? index + 2 : index - 1]
+        console.log(index);
+        if (index == 2) {
+            btnBanner.style.background = colorsTwo[index == 0 ? 2 : index - 1]
+            btnBanner.style.boxShadow = `0 0 5px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 25px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 50px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 100px ${colorsTwo[index == 0 ? 2 : index - 1]}`
+        } else {
+            btnBanner.style.background = colorsTwo[index == 0 ? 2 : index - 1]
+            btnBanner.style.boxShadow = `0 0 5px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 25px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 50px ${colorsTwo[index == 0 ? 2 : index - 1]}, 0 0 100px ${colorsTwo[index == 0 ? 2 : index - 1]}`
+        }
     })
 
     if (index == 2) {
@@ -115,7 +140,47 @@ const changeBackgroundContact = () => {
     }
 }
 
+const changeBackgroundLogin = () => {
+    const main = document.querySelector('main')
+    const btnColor = document.getElementById('btnColor')
+    const footer = document.querySelector('footer')
+    const header = document.querySelector('header')
+    const h1 = document.querySelectorAll('h1')
+    const btn = document.getElementById('btn')
 
+    btnColor.style.background = colorsOne[index]
+    main.style.background = colorsOne[index]
+    footer.style.background = colorsThree[index]
+    header.style.background = colorsThree[index]
+    btn.style.background = colorsContact[index]
+    h1[1].style.color = colorsContact[index]
+
+    const input = document.querySelectorAll('.input')
+    const label = document.querySelectorAll('.label')
+    input[0].addEventListener('focus', () => {
+        label[0].style.color = colorsContact[index == 0 ? index : index - 1];
+    })
+    input[1].addEventListener('focus', () => {
+        label[1].style.color = colorsContact[index == 0 ? index : index - 1];
+    })
+
+    input[0].addEventListener('blur', () => {
+        label[0].style.color = '#000';
+    })
+    input[1].addEventListener('blur', () => {
+        label[1].style.color = '#000';
+    })
+
+    if (index == 2) {
+        btnColor.style.color = "#000000"
+        btnColor.style.border = "2px solid #000000"
+        index = 0
+    } else {
+        btnColor.style.border = "2px solid #ffff"
+        btnColor.style.color = "#FFFFFF"
+        index++
+    }
+}
 
 const slideShow = () => {
     const banner = document.getElementById('banner')
@@ -136,5 +201,6 @@ const slideShow = () => {
     setTimeout(slideShow, time)
 }
 slideShow()
+
 
 
